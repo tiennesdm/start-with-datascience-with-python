ef70ebbab93cfd12895d8aa0d9f2365b6c31959a
Shubham Mehta <s.mehta@blockcore.ltd>
2019-03-04 16:56:44 +0530

    numpy

---
+++ Numpy lib. practice/About+numpy.py
@@ -0,0 +1,495 @@
+
+# coding: utf-8
+
+# In[4]:
+
+import numpy as np
+arr = np.arange(1,5)
+
+print(arr)
+
+
+
+
+# # numpy library
+# 
+
+# In[8]:
+
+import numpy as np
+arr = np.arange(5)
+print (arr)
+
+
+# In[9]:
+
+list = [1,2,3,4]
+arr=np.array(list)
+print(arr)
+
+
+# In[10]:
+
+list = [[1,2,6],[4,5,6]]
+arr=np.array(list)
+print(arr)
+
+
+# In[11]:
+
+np.arrange(10)
+
+
+# In[13]:
+
+import numpy as np
+np.arange(10)
+
+
+# In[15]:
+
+##arrange
+import numpy as np
+np.arange(0,6)
+
+
+# In[19]:
+
+#arange slice
+import numpy as np 
+np.arange(2,6,2)
+
+
+# In[20]:
+
+#zeros and ones
+import numpy as np 
+np.zeros(3)
+
+
+# In[21]:
+
+#zeros for 2d
+import numpy as np 
+np.zeros((3,3))
+
+
+# In[25]:
+
+#zeros for 2d with multiple
+import numpy as np 
+np.zeros((3,3))*2
+
+
+# In[26]:
+
+#ones for 1d
+import numpy as np
+np.ones(5)
+
+
+# In[28]:
+
+#ones for 2d 
+import numpy as np 
+np.ones((2,1))
+
+
+# In[29]:
+
+#ones with multiple
+import numpy as np 
+np.ones((2,2))*3
+
+
+# In[30]:
+
+#ones with sum
+import numpy as np 
+np.ones((2,2))+3
+
+
+# In[31]:
+
+#ones with power 
+import numpy as np 
+np.ones((2,2))**2
+
+
+# In[32]:
+
+#linspace 
+import numpy as np 
+np.linspace(0,5)
+
+
+# In[36]:
+
+#linspace 
+import numpy as np 
+np.linspace(0,5)*10
+
+
+# In[37]:
+
+#numpy eye 
+import numpy as np 
+np.eye(4)
+
+
+# In[38]:
+
+np.eye(4,4)
+
+
+# In[39]:
+
+np.eye(5)
+
+
+# In[40]:
+
+#random 
+import numpy as np
+np.random.randn(2)
+
+
+# In[41]:
+
+#random int 
+np.random.randint(5)
+
+
+# In[42]:
+
+#random int numbers
+np.random.randint(1,1000,5)
+
+
+# In[43]:
+
+#random array standard normal deviation
+np.random.randn(5,5)
+
+
+# In[45]:
+
+#array with reshape
+import numpy as np 
+arr = np.arange(20)
+print(arr)
+
+
+# In[46]:
+
+arr.reshape(5,4)
+
+
+# In[47]:
+
+#max
+arr.max()
+
+
+# In[48]:
+
+#min
+arr.min()
+
+
+# In[50]:
+
+#shape
+arr.shape
+
+
+# In[51]:
+
+#arr reshape
+arr.reshape(20,1)
+
+
+# In[53]:
+
+#date type for the array
+arr.dtype
+
+
+# In[56]:
+
+#indexing and slicing 
+import numpy as np 
+arr=np.arange(1,5)
+print(arr)
+
+
+# In[57]:
+
+arr[2]
+
+
+# In[58]:
+
+arr[0:2]
+
+
+# In[59]:
+
+arr[0:]
+
+
+# In[60]:
+
+arr[-1:]
+
+
+# In[61]:
+
+arr[-1:0]
+
+
+# In[63]:
+
+arr_copy = arr.copy()
+print(arr_copy)
+
+
+# In[69]:
+
+#selection
+z=arr>3
+y=arr<3
+x= arr=3
+print(x,y,z)
+
+
+# In[71]:
+
+x=2
+print(arr)
+
+
+# In[82]:
+
+#numpy operation
+import numpy as np 
+arr= np.arange(0,10,2)
+print(arr)
+print(arr+arr)
+print(arr*arr)
+print(arr-arr)
+print(arr**2)
+print(arr**3)
+
+
+# In[83]:
+
+#sin functions
+import numpy as np
+np.sin(arr)
+
+
+# In[84]:
+
+#log functions
+import numpy as np 
+np.log(arr)
+
+
+# In[85]:
+
+#multidimensional array 
+import numpy as np 
+arr= np.arange(25).reshape(5,5)
+print(arr)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+#
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
